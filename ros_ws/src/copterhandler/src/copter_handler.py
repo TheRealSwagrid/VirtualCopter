8125feab9ada384e357de319dfa7157972c5b42d
rosrunner <luca.alfano@uni-a.de>
2023-11-16 16:36:59 +0100

    New Functionality

--- ros_ws/src/copterhandler/src/copter_handler.py
+++ ros_ws/src/copterhandler/src/copter_handler.py
@@ -17,7 +17,7 @@ class CopterHandler:
 
         self.position = np.array([0, 0, 2.])
         self.rotation = [0, 0, 0, 1]
-        self.scale = .2
+        self.scale = 1.5
 
         self.tf_position = None
         self.max_vel = .1
@@ -100,7 +100,7 @@ class CopterHandler:
                 self.position += current_vel
                 self.publish_visual()
                 # TODO wait appropriate
-                sleep(np.sum(np.abs(current_vel)) * .1)
+                sleep(np.sum(np.abs(current_vel)) * .01)
                 #sleep((abs(current_vel[0]) + abs(current_vel[1]) + abs(current_vel[2])))
                 vel += self.acc
                 vel = min(vel, self.max_vel)
