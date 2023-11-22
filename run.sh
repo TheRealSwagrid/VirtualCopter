9f0c3547d937bfb9eb0a7e2d2d6bb208ee83a71f
rosrunner <luca.alfano@uni-a.de>
2023-11-22 11:01:39 +0100

    added run sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 port="$1"
-cp VirtualCopter.py /ros_ws/src/copterhandler/src
+cp VirtualCopter.py ros_ws/src/copterhandler/src
 cp AbstractVirtualCapability.py ros_ws/src/copterhandler/src
-cd /ros_ws && source /opt/ros/noetic/setup.bash && catkin_make
-source /ros_ws/devel/setup.bash && roslaunch copterhandler copterhandler.launch semantix_port:="$port"
+cd ros_ws && source /opt/ros/noetic/setup.bash && catkin_make
+source devel/setup.bash && roslaunch copterhandler copterhandler.launch semantix_port:="$port"
