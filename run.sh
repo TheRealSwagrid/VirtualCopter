c829f6d0040ad5b58b9539278e2c647e5ee686d5
rosrunner <luca.alfano@uni-a.de>
2023-11-29 14:25:27 +0100

    modified run sh

--- run.sh
+++ run.sh
@@ -5,3 +5,6 @@ cp VirtualCopter.py ros_ws/src/copterhandler/src
 cp AbstractVirtualCapability.py ros_ws/src/copterhandler/src
 cd ros_ws && source /opt/ros/noetic/setup.bash && catkin_make
 source devel/setup.bash && roslaunch copterhandler copterhandler.launch semantix_port:="$port"
+sudo rm -R build
+sudo rm -R devel
+rm .catkin_workspace
